75b8b258397e9d4dd012b2ea7ad7b6bc522b4389
LATIFADEV <latifaaamouguay@gmail.com>
2022-06-07 11:52:20 +0200

    add pro

---
+++ Brief7-MAVEN-JAVA-PROJECT-INSCREPTION-POSTGRESQL-JAVAFAX/src/com/inscreption/Test.java
@@ -0,0 +1,5 @@
+package com.inscreption;
+
+public class Test {
+
+}
---
+++ mmm/src/mmm/Test1.java
@@ -0,0 +1,10 @@
+package mmm;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello ");
+
+	}
+
+}
